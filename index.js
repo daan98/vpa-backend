80dce7ee8d23fff943ca93fea86d192d7c75fcdf
daan98 <daan98.da@gmail.com>
2023-06-05 14:28:12 -0600

    comment the use of cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const corsOptions = {
   },
 };
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 app.use("/api/veterinarian", veterinarianRoutes);
 app.use("/api/patient", patientRoutes);
 
